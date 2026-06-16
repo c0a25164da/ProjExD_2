f5c46728665809cb1e02f3b90f5aa55545df851c
tatsuki <c0a25164da@edu.teu.ac.jp>
2026-06-16 14:23:45 +0900

    練習2：動的爆弾追加

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -20,6 +21,12 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20, 20))
+    bb_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -40,10 +47,12 @@ def main():
         #     sum_mv[0] += 5
         for key, mv in DELTA.items():
             if key_lst[key]:
-                sum_mv[0] += mv[0] # 横方向移動
-                sum_mv[1] += mv[1] # 縦方向移動
+                sum_mv[0] += mv[0]  # 横方向移動
+                sum_mv[1] += mv[1]  # 縦方向移動
         kk_rct.move_ip(sum_mv)
+        bb_rct.move_ip(vx, vy)
         screen.blit(kk_img, kk_rct)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
